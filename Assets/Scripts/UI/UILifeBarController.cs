c35b3d18991af4ff4e193e041c556247d2dac1ac
Deki.Senpai <dekisenpaitm@gmail.com>
2026-04-19 23:34:50 +0200

    Fucking fuckt fucking life fucking bar
    hurns farben change geht net,

---
+++ Assets/Scripts/UI/UILifeBarController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class UILifeBarController : MonoBehaviour
+{
+    [Header("LifeBarElements")]
+    public List<UILifeBarElement> lifeBarElements;
+
+    public void SetCurrentHealth(int amount)
+    {
+        for (int i = 0; i < lifeBarElements.Count - amount; i++)
+        {
+            lifeBarElements[i].setEmpty(true);
+        }
+
+        for(int i = lifeBarElements.Count - amount; i < lifeBarElements.Count; i++)
+        {
+            lifeBarElements[i].setEmpty(false);
+        }
+    }
+}
