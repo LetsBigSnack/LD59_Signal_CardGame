1b547368be192834b6b0e520b609162b15b34170
Deki.Senpai <dekisenpaitm@gmail.com>
2026-04-20 23:22:20 +0200

    fixed ui button

--- Assets/Scripts/UI/UICardAdd.cs
+++ Assets/Scripts/UI/UICardAdd.cs
@@ -8,20 +8,14 @@ public class UICardAdd : UICard
     [SerializeField]
     public GameObject button;
 
-    private bool hasBeenClicked = false;
-
     public override void OnPointerEnter(PointerEventData eventData)
     {
-        if (hasBeenClicked) return;
         button.SetActive(true);
     }
 
     public void Update()
     {
-        if (hasBeenClicked)
-        {
-            button.SetActive(false);
-        }
+        button.SetActive(false);
     }
 
     public override void OnPointerExit(PointerEventData eventData)
@@ -32,6 +26,5 @@ public class UICardAdd : UICard
     public void SelectCardToAddToDeck()
     {
         RewardManager.Instance.SelectReward(GetCardData());
-        hasBeenClicked = true;
     }
 }
